3d7c504b8cb40d84b0d3a967e22ed0d91ffb4d8d
Dmitriy Gulyaev <80756547+Moxckerry@users.noreply.github.com>
2021-04-05 19:12:08 +0400

    Update connect.sql

--- connect.sql
+++ connect.sql
@@ -27,10 +27,21 @@ insert into users (name, last_name, email)
 	('Ivan','Beregovskiy','ivanberegovskiy@mail.ru'),
 	('Pavel','Korchagin','pavelkorchagin@mail.com'),
 	('Kirill','Lysov','lysovkirill@mail.com'),
-    ('Dmitriy','Katkov','dmitriykatkov@mail.ru'),
-    ('Vladislav','Nazarov','vladnazarov@gmail.com'),
-    ('Nikita','Sursar','nikitasursar@gmail.com'),
-    ('Arseniy','Kitaev','arseniykitaev@gmail.com'),
-    ('Filipp','Lui','luifilipp@gmail.com');
+    	('Dmitriy','Katkov','dmitriykatkov@mail.ru'),
+    	('Vladislav','Nazarov','vladnazarov@gmail.com'),
+    	('Nikita','Sursar','nikitasursar@gmail.com'),
+   	('Arseniy','Kitaev','arseniykitaev@gmail.com'),
+   	('Filipp','Lui','luifilipp@gmail.com');
 	
-select * from users;
+select id, name, last_name from users where name = 'Dmitriy' and id >= 5 limit 1;
+
+update users set name = 'Maksim' and last_name = 'Sergeev' and email = 'mihailsergeev@gmail.com' where id = 6;
+
+select email from users where id in (1,3,5,7,9);
+
+delete from users where id = 7;
+
+insert into users (name, last_name, email) 
+values ('Sergey', 'Stepanets', 'sergeystepanets@mail.ru');
+
+select name, last_name where (id <= 2 or id >= 6) and name = 'Maksim';
