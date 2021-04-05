9b03f9e3025f624862f962ea7240484dd43a0fef
Dmitriy Gulyaev <80756547+Moxckerry@users.noreply.github.com>
2021-04-05 18:41:05 +0400

    Update connect.sql

--- connect.sql
+++ connect.sql
@@ -1,25 +1,36 @@
 create table users(
-    id serial primary key ,
-    name varchar(255) not null,
-    last_name varchar(255) not null,
-    email varchar(255) unique not null,
+    id serial primary key,
+    name varchar(255),
+    last_name varchar(255),
+    email varchar(255),
     work_id int
 );
 
 create table works(
     id serial primary key ,
-    work_name varchar(255) not null,
+    work_name varchar(255),
     status bool
 );
 
 create table timings(
     id serial primary key,
-    code varchar(255) not null,
-    track uuid,
+    code varchar(255),
+    track int,
+    user_id int,
     work_id int
+
 );
 
 insert into users (name, last_name, email)
-    VALUES ('Dmitriy', 'Gulyaev', 'dmitriiguliaev@mail.ru');
-
-select id, name, last_name, email from users;
+    values ('Dmitriy', 'Gulyaev', 'dmitriygulyaev@mail.ru'),
+	('Maksim','Anikin','maksimanikin@mail.ru'),
+	('Ivan','Beregovskiy','ivanberegovskiy@mail.ru'),
+	('Pavel','Korchagin','pavelkorchagin@mail.com'),
+	('Kirill','Lysov','lysovkirill@mail.com'),
+    ('Dmitriy','Katkov','dmitriykatkov@mail.ru'),
+    ('Vladislav','Nazarov','vladnazarov@gmail.com'),
+    ('Nikita','Sursar','nikitasursar@gmail.com'),
+    ('Arseniy','Kitaev','arseniykitaev@gmail.com'),
+    ('Filipp','Lui','luifilipp@gmail.com');
+	
+select * from users;
